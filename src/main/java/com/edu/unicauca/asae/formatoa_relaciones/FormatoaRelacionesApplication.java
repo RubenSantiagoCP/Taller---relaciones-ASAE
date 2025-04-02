24c4f3f4e5dea8a409e9ffce7df8ab537abe58f4
rscruz <santiagocrzzz1711@gmail.com>
2025-04-02 11:41:24 -0500

    dependencies

---
+++ src/main/java/com/edu/unicauca/asae/formatoa_relaciones/FormatoaRelacionesApplication.java
@@ -0,0 +1,13 @@
+package com.edu.unicauca.asae.formatoa_relaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormatoaRelacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormatoaRelacionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/unicauca/asae/formatoa_relaciones/FormatoaRelacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.unicauca.asae.formatoa_relaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormatoaRelacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
